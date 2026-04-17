30a7715f9be5d03f50acffd2c83b691962519ef7
jb <jb239812@ohio.edu>
2026-04-17 17:15:03 -0400

    a few more common utilities in the common shader header

--- shaders/common.h
+++ shaders/common.h
@@ -33,7 +33,8 @@ layout( set = 0, binding = 0 ) uniform globalData {
 
 #ifndef PI_DEFINED
 #define PI_DEFINED
-const float pi = 3.141592f;
+const float pi = 3.1415926535f;
+const float piHalf = pi / 2.0f;
 const float tau = 2.0f * pi;
 const float sqrtpi = 1.7724538509f;
 #endif
@@ -42,5 +43,30 @@ const float sqrtpi = 1.7724538509f;
 #define REMAP_DEFINED
 float remap ( float value, float inLow, float inHigh, float outLow, float outHigh ) {
 	return outLow + ( value - inLow ) * ( outHigh - outLow ) / ( inHigh - inLow );
+}
+#endif
+
+#ifndef BASIC_ROTATIONS_DEFINED
+#define BASIC_ROTATIONS_DEFINED
+mat2 Rotate2D ( in float a ) {
+	float c = cos( a ), s = sin( a );
+	return mat2( c, s, -s, c );
+}
+mat3 Rotate3D ( const float angle, const vec3 axis ) {
+	const vec3 a = normalize( axis );
+	const float s = sin( angle );
+	const float c = cos( angle );
+	const float r = 1.0f - c;
+	return mat3(
+		a.x * a.x * r + c,
+		a.y * a.x * r + a.z * s,
+		a.z * a.x * r - a.y * s,
+		a.x * a.y * r - a.z * s,
+		a.y * a.y * r + c,
+		a.z * a.y * r + a.x * s,
+		a.x * a.z * r + a.y * s,
+		a.y * a.z * r - a.x * s,
+		a.z * a.z * r + c
+	);
 }
 #endif
