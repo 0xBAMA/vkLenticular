9203a9c5bb7b9e8c8b533ecd1d83a67a99bee52a
jb <jb239812@ohio.edu>
2026-04-18 00:26:56 -0400

    shader box trace working

--- shaders/outputResolve.comp.glsl
+++ shaders/outputResolve.comp.glsl
@@ -5,62 +5,71 @@
 
 layout ( local_size_x = 16, local_size_y = 16 ) in;
 
+#include "common.h"
+
 layout ( rgba32f, set = 0, binding = 1 ) uniform image2D lenticularLUT;
 layout ( rgba32f, set = 0, binding = 2 ) uniform image2D accumulator;
 
-#include "common.h"
-
-const float maxDist = 1e10f;
-const vec2 distBound = vec2( 0.0001f, maxDist );
-
-float RayBoxIntersect ( in vec3 ro, in vec3 rd, in vec3 boxSize, in vec3 boxPosition ) {
-	ro -= boxPosition;
-
-	vec3 m = sign( rd ) / max( abs( rd ), 1e-8 );
-	vec3 n = m * ro;
-	vec3 k = abs( m ) * boxSize;
-
-	vec3 t1 = -n - k;
-	vec3 t2 = -n + k;
-
-	float tN = max( max( t1.x, t1.y ), t1.z );
-	float tF = min( min( t2.x, t2.y ), t2.z );
-
-	if ( tN > tF || tF <= 0.0f ) {
-		return maxDist;
-	} else {
-		if ( tN >= distBound.x && tN <= distBound.y ) {
-			return tN;
-		} else if ( tF >= distBound.x && tF <= distBound.y ) {
-			return tF;
-		} else {
-			return maxDist;
-		}
-	}
+float tMin, tMax; // global state tracking
+bool Intersect ( const vec3 rO, vec3 rD ) {
+	// Intersect() code adapted from:
+	//    Amy Williams, Steve Barrus, R. Keith Morley, and Peter Shirley
+	//    "An Efficient and Robust Ray-Box Intersection Algorithm"
+	//    Journal of graphics tools, 10(1):49-54, 2005
+	const float minDistance = -100.0;
+	const float maxDistance =  100.0;
+	int s[ 3 ]; // sign toggle
+	// inverse of ray direction
+	const vec3 iD = vec3( 1.0 ) / rD;
+	s[ 0 ] = ( iD[ 0 ] < 0 ) ? 1 : 0;
+	s[ 1 ] = ( iD[ 1 ] < 0 ) ? 1 : 0;
+	s[ 2 ] = ( iD[ 2 ] < 0 ) ? 1 : 0;
+	const vec3 min = vec3( -1.0, -1.0, -1.0 );
+	const vec3 max = vec3(  1.0,  1.0,  1.0 );
+	const vec3 b[ 2 ] = { min, max }; // bounds
+	tMin = ( b[ s[ 0 ] ][ 0 ] - rO[ 0 ] ) * iD[ 0 ];
+	tMax = ( b[ 1 - s[ 0 ] ][ 0 ] - rO[ 0 ] ) * iD[ 0 ];
+	const float tYMin = ( b[ s[ 1 ] ][ 1 ] - rO[ 1 ] ) * iD[ 1 ];
+	const float tYMax = ( b[ 1 - s[ 1 ] ][ 1 ] - rO[ 1 ] ) * iD[ 1 ];
+	if ( ( tMin > tYMax ) || ( tYMin > tMax ) ) return false;
+	if ( tYMin > tMin ) tMin = tYMin;
+	if ( tYMax < tMax ) tMax = tYMax;
+	const float tZMin = ( b[ s[ 2 ] ][ 2 ] - rO[ 2 ] ) * iD[ 2 ];
+	const float tZMax = ( b[ 1 - s[ 2 ] ][ 2 ] - rO[ 2 ] ) * iD[ 2 ];
+	if ( ( tMin > tZMax ) || ( tZMin > tMax ) ) return false;
+	if ( tZMin > tMin ) tMin = tZMin;
+	if ( tZMax < tMax ) tMax = tZMax;
+	return ( ( tMin < maxDistance ) && ( tMax > minDistance ) );
 }
 
 void main () {
-	ivec2 idx = ivec2( gl_GlobalInvocationID.xy );
+	ivec2 idx = ivec2(gl_GlobalInvocationID.xy);
 
-	vec2 uv = 2.0f * ( idx + vec2( 0.5f ) ) / vec2( imageSize( accumulator ).xy ) - vec2( 1.0f );
-	uv.x *= float( imageSize( accumulator ).x ) / float( imageSize( accumulator ).y );
+	vec2 uv = (2.0f * (vec2(idx) + vec2(0.5f)) / vec2(imageSize(accumulator).xy)) - vec2(1.0f);
+	uv.x *= float(imageSize(accumulator).x) / float(imageSize(accumulator).y);
 
 	// create a view ray
-	 vec3 rayOrigin = GlobalData.zoomFactor * ( uv.x * GlobalData.viewBasisX + uv.y * GlobalData.viewBasisY ) - 10.0f * GlobalData.viewBasisZ;
+	vec3 rayOrigin = GlobalData.zoomFactor * (uv.x * GlobalData.viewBasisX + uv.y * GlobalData.viewBasisY) - 10.0f * GlobalData.viewBasisZ;
+	vec3 rayDirection = GlobalData.viewBasisZ;
 
-	// primary ray intersects with the bounds...
-	float dBounds = RayBoxIntersect( rayOrigin, GlobalData.viewBasisZ, vec3( 1.0f ), vec3( 0.0f ) );
+	vec4 color = vec4( 0.0f );
+//	if ( GlobalData.reset != 0 ) {
+//		 handling image reset, here - cancel history and only write this frame's data
+//		color = vec4( 0.0f, 0.0f, 0.0f, 1.0f );
+//	} else
+	if ( Intersect( rayOrigin, rayDirection ) ) {
+		// if the ray hits, we're pathtracing
+		color.xyz = vec3( 1.0f / tMin );
 
-	vec3 color = ( dBounds == maxDist ) ? ( vec3( 1.0f, 0.0f, 0.0f ) ) : ( vec3( 1.0f / dBounds ) );
-	// vec3 color = vec3( uv, 0.0f );
-//	vec3 color = rayOrigin;
 
-	// if the ray hits, we're pathtracing
 
-	// else, we're taking a black sample
+//		vec4 prevColor = imageLoad( accumulator, idx );
+//		float sampleCount = max( prevColor.a, 0.0f ) + 1.0f;
+//		const float mixFactor = saturate( 1.0f / sampleCount );
+//		color = vec4( mix( prevColor.rgb, color.rgb, mixFactor ), sampleCount );
+//	} else {
+//		color = vec4( 0.0f, 0.0f, 0.0f, 1.0f );
+	}
 
-	// writing a result back to the accumulator...
-		// handling image reset, here - cancel history and only write this frame's data
-
-	imageStore( accumulator, idx, vec4( color, 1.0f ) );
+	imageStore( accumulator, idx, color );
 }
