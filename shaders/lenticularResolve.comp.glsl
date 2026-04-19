445cead2857b4dc39382a89bb4687a830f8a8d6f
jb <jb239812@ohio.edu>
2026-04-18 22:22:06 -0400

    experimenting with SDFs

--- shaders/lenticularResolve.comp.glsl
+++ shaders/lenticularResolve.comp.glsl
@@ -8,6 +8,7 @@ layout ( local_size_x = 16, local_size_y = 16 ) in;
 layout ( rgba8, set = 0, binding = 1 ) uniform image2D lenticularLUT;
 
 #include "common.h"
+#include "hg_sdf.h"
 
 vec2 RaySphereIntersect ( vec3 r0, vec3 rd, vec3 s0, float sr ) {
 	// r0 is ray origin
@@ -25,6 +26,50 @@ vec2 RaySphereIntersect ( vec3 r0, vec3 rd, vec3 s0, float sr ) {
 	} else {
 		return vec2( -b - sqrt( disc ), -b + sqrt( disc ) ) / ( 2.0f * a );
 	}
+}
+
+float de( vec3 p ){
+	// spatial repeats
+	pMod3( p, vec3( 5.0f ) );
+
+	float s = 2.;
+	float e = 0.;
+	for(int j=0;++j<7;)
+	p.xz=abs(p.xz)-2.3,
+	p.z>p.x?p=p.zyx:p,
+	p.z=1.5-abs(p.z-1.3+sin(p.z)*.2),
+	p.y>p.x?p=p.yxz:p,
+	p.x=3.-abs(p.x-5.+sin(p.x*3.)*.2),
+	p.y>p.x?p=p.yxz:p,
+	p.y=.9-abs(p.y-.4),
+	e=12.*clamp(.3/min(dot(p,p),1.),.0,1.)+
+	2.*clamp(.1/min(dot(p,p),1.),.0,1.),
+	p=e*p-vec3(7,1,1),
+	s*=e;
+	return length(p)/s;
+}
+
+const float epsilon = 0.001f;
+bool didHit = false;
+float raymarch ( vec3 rayOrigin, vec3 rayDirection ) {
+	float dQuery = 0.0f;
+	float dTotal = 0.0f;
+	vec3 pQuery = rayOrigin;
+	for ( int steps = 0; steps < 200; steps++ ) {
+		pQuery = rayOrigin + dTotal * rayDirection;
+		dQuery = de( pQuery );
+		dTotal += dQuery * 0.9f;
+		didHit = ( abs( dQuery ) < epsilon );
+		if ( dTotal > 20.0f || didHit ) {
+			break;
+		}
+	}
+	return dTotal;
+}
+
+vec3 SDFNormal ( in vec3 position ) {
+	vec2 e = vec2( epsilon, 0.0f );
+	return normalize( vec3( de( position ) ) - vec3( de( position - e.xyy ), de( position - e.yxy ), de( position - e.yyx ) ) );
 }
 
 void main () {
@@ -52,6 +97,23 @@ void main () {
 	if ( roots != vec2( -1.0f ) ) {
 		vec3 p = rayOrigin + rayDirection * roots.x;
 		color = normalize( p - sphereCenter ) * step( mod( p.z, 0.2f ), 0.1f );
+	float dRaymarch = raymarch( rayOrigin + vec3( 0.0f, 0.0f, 0.75f ), rayDirection );
+	if ( didHit ) {
+		const vec3 normal = SDFNormal( rayOrigin + dRaymarch * rayDirection );
+		const vec3 pHit = rayOrigin + dRaymarch * rayDirection + 3.0f * epsilon * normal;
+		const vec3 pLight = vec3( 0.0f );
+		const vec3 vLight = pLight - pHit;
+		const vec3 vLightNorm = normalize( vLight );
+
+		color = vec3( 0.75f, 0.5f, 0.3f );
+
+		// shadow trace
+		if ( raymarch( pHit, vLightNorm ) < length( vLight ) ) {
+			// we hit something...
+			color *= 0.1f;
+		} else {
+			color *= saturate( dot( vLightNorm, normal ) );
+		}
 	}
 
 	// we need to store back the result, into the lenticular LUT
